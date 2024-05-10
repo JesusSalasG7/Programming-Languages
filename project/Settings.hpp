45184f96f7931e52e7122644cf2bd8fccb8535a8
DefFoxPy <66576421+DefFoxPy@users.noreply.github.com>
2024-05-09 22:35:05 -0400

    feat: add the three sections

--- project/Settings.hpp
+++ project/Settings.hpp
@@ -2,8 +2,14 @@
 
 struct Settings
 {
-    static constexpr int WINDOW_WIDTH{1280};
-    static constexpr int WINDOW_HEIGHT{720};
+    static constexpr int WINDOW_WIDTH{640};
+    static constexpr int WINDOW_HEIGHT{480};
+    static constexpr int SECTION1_WIDTH{WINDOW_WIDTH - WINDOW_WIDTH / 4};
+    static constexpr int SECTION1_HEIGHT{WINDOW_HEIGHT - WINDOW_HEIGHT / 4};
+    static constexpr int SECTION2_WIDTH{WINDOW_WIDTH / 4};
+    static constexpr int SECTION2_HEIGHT{WINDOW_HEIGHT - WINDOW_HEIGHT / 4};
+    static constexpr int SECTION3_WIDTH{WINDOW_WIDTH};
+    static constexpr int SECTION3_HEIGHT{WINDOW_HEIGHT / 4};
 
     static void init();
     static void load_textures();
--- project/main.cpp
+++ project/main.cpp
@@ -6,7 +6,22 @@ int main()
 {   
     Settings::init();
 
-    sf::Window window{sf::VideoMode{Settings::WINDOW_WIDTH, Settings::WINDOW_HEIGHT}, "languages", sf::Style::Close};
+    sf::RenderWindow window{sf::VideoMode{Settings::WINDOW_WIDTH, Settings::WINDOW_HEIGHT}, "languages", sf::Style::Close};
+
+    // Definir los rectángulos para las tres secciones
+    sf::RectangleShape section1(sf::Vector2f(Settings::SECTION1_WIDTH, Settings::SECTION1_HEIGHT));
+    sf::RectangleShape section2(sf::Vector2f(Settings::SECTION2_WIDTH, Settings::SECTION2_HEIGHT));
+    sf::RectangleShape section3(sf::Vector2f(Settings::SECTION3_WIDTH, Settings::SECTION3_HEIGHT));
+
+    // Establecer las posiciones de las secciones
+    section1.setPosition(0, 0);
+    section2.setPosition(Settings::SECTION1_WIDTH, 0); 
+    section3.setPosition(0, Settings::SECTION1_HEIGHT);
+
+    // Establecer colores diferentes para cada sección
+    section1.setFillColor(sf::Color::Red);
+    section2.setFillColor(sf::Color::Green);
+    section3.setFillColor(sf::Color::Blue);
 
     while (window.isOpen())
     {
@@ -19,6 +34,14 @@ int main()
                 window.close();
             }
         }
+
+        window.clear(sf::Color::Black);
+
+        window.draw(section1);
+        window.draw(section2);
+        window.draw(section3);
+
+        window.display();
     }
 
     return EXIT_SUCCESS;
